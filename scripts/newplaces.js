00d5a05150c0927c3c8f46a233659dae673d4e79
harshaval <sriharsha.valluru@gmail.com>
2023-08-30 15:52:18 +0100

    push final

--- scripts/newplaces.js
+++ scripts/newplaces.js
@@ -1,50 +1,89 @@
+
 window.onload = () => {
-    let downloaded = false;
+    const button = document.querySelector('button[data-action="change"]');
+    button.innerText = '﹖';
 
-    const cam = document.querySelector("[gps-new-camera]");
-
-    cam.addEventListener("gps-camera-update-position", async(e) => {
-        if(!downloaded) {
-            const west = e.detail.position.longitude - 0.05,
-                  east = e.detail.position.longitude + 0.05,
-                  south = e.detail.position.latitude - 0.05,
-                  north = e.detail.position.latitude + 0.05;
-            console.log(`${west} ${south} ${east} ${north}`);
-            const response = await fetch(`https://hikar.org/webapp/map?bbox=${west},${south},${east},${north}&layers=poi&outProj=4326`);
-            const pois = await response.json();
-            pois.features.forEach ( feature => {
-                const compoundEntity = document.createElement("a-entity");
-                compoundEntity.setAttribute('gps-new-entity-place', {
-                    latitude: feature.geometry.coordinates[1],
-                    longitude: feature.geometry.coordinates[0]
-                });
-                const box = document.createElement("a-box");
-                box.setAttribute("scale", {
-                    x: 20,
-                    y: 20,
-                    z: 20
-                });
-                box.setAttribute('material', { color: 'red' } );
-                box.setAttribute("position", {
-                    x : 0,
-                    y : 50,
-                    z: 0
-                } );
-                const label = document.createElement("a-text");
-                const textScale = 100;
-                label.setAttribute("look-at", "[gps-new-camera]");
-                label.setAttribute("scale", {
-                    x: textScale,
-                    y: textScale,
-                    z: textScale
-                });
-                label.setAttribute("value", feature.properties.name);
-                label.setAttribute("align", "center");
-                compoundEntity.appendChild(box);
-                compoundEntity.appendChild(label);
-                document.querySelector("a-scene").appendChild(compoundEntity);
-            });
-        }
-        downloaded = true;
-    });
+    let places = staticLoadPlaces();
+    renderPlaces(places);
 };
+
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'Home',
+            location: {
+                // decomment the following and add coordinates:
+                lat: 53.221641,
+                lng: -4.130223,
+            },
+        },
+        {
+            name: 'Barlows',
+            location: {
+                // decomment the following and add coordinates:
+                lat: 53.222303686325006,
+                lng: -4.129229144041981,
+            },
+        }
+    ];
+}
+
+var models = [
+    {
+        url: './assets/map-pin.gltf',
+        scale: '0.5 0.5 0.5',
+        info: 'Home marker',
+        rotation: '0 180 0',
+    },
+    {
+        url: './assets/pin.gltf',
+        scale: '0.2 0.2 0.2',
+        rotation: '0 180 0',
+        info: 'Barlows marker',
+    },
+];
+
+var modelIndex = 0;
+var setModel = function (model, entity) {
+    if (model.scale) {
+        entity.setAttribute('scale', model.scale);
+    }
+
+    if (model.rotation) {
+        entity.setAttribute('rotation', model.rotation);
+    }
+
+    if (model.position) {
+        entity.setAttribute('position', model.position);
+    }
+
+    entity.setAttribute('gltf-model', model.url);
+
+    const div = document.querySelector('.instructions');
+    div.innerText = model.info;
+};
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+
+        setModel(models[modelIndex], model);
+
+        model.setAttribute('animation-mixer', '');
+
+        document.querySelector('button[data-action="change"]').addEventListener('click', function () {
+            var entity = document.querySelector('[gps-entity-place]');
+            modelIndex++;
+            var newIndex = modelIndex % models.length;
+            setModel(models[newIndex], entity);
+        });
+
+        scene.appendChild(model);
+    });
+}
